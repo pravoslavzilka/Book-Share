de9e7916363c13abf97042e0274b1cb53ac34d6f
Pravoslav <pravoslav.zilka@gmail.com>
2021-05-23 03:05:58 +0200

    adding new student feature

--- blueprints/student/__init__.py
+++ blueprints/student/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
 from database import db_session
 from models import Grade, Student
 
@@ -15,8 +15,18 @@ def landing_page():
 
 @student_bp.route("/landing_page/<grade>/")
 def landing_page_grade(grade):
-
     grades = Grade.query.all()
     cer_grade = Grade.query.filter(Grade.name == grade).first()
     print(cer_grade)
     return render_template("student/landing_page.html", grades=grades, students=cer_grade.students)
+
+
+@student_bp.route("/new_student",methods=["POST"])
+def new_student():
+    name = request.form["student_name"]
+    s_grade = request.form["grade"]
+    grade = Grade.query.filter(Grade.name == s_grade).first()
+    n_student = Student(name,grade)
+    db_session.add(n_student)
+    db_session.commit()
+    return redirect(url_for("student_bp.landing_page"))
